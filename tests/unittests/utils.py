4f75fcb037a4476f9b01cd3619f8fb524f0e4d35
Matěj Týč <matej.tyc@gmail.com>
2014-11-24 09:02:27 +0100

    Added filtering tests

--- tests/unittests/utils.py
+++ tests/unittests/utils.py
@@ -58,6 +58,39 @@ class TestUtils(ut.TestCase):
             self.assertEqual(what.shape, undone.shape)
             np.testing.assert_equal(what, undone)
 
+    def _addFreq(self, src, vec):
+        dom = np.zeros(src.shape)
+        dom += np.arange(src.shape[0])[:, np.newaxis] * np.pi * vec[0]
+        dom += np.arange(src.shape[1])[np.newaxis, :] * np.pi * vec[1]
+
+        src += np.sin(dom)
+
+        return src
+
+    @staticmethod
+    def _arrdiff(a, b):
+        adiff = np.abs(a - b)
+        ret = adiff.mean(), adiff.max()
+        return ret
+
+    def _wrapFilter(self, src, vecs, * args):
+        dest = src.copy()
+        for vec in vecs:
+            self._addFreq(dest, vec)
+
+        filtered = utils.imfilter(dest, * args)
+        mold, mnew = [self._arrdiff(src, arr)[0] for arr in (dest, filtered)]
+        self.assertGreater(mold * 1e-10, mnew)
+
+    def testFilter(self):
+        src = np.zeros((20, 30))
+
+        self._wrapFilter(src, [(0.8, 0.8)], (0.8, 1.0))
+        self._wrapFilter(src, [(0.1, 0.2)], None, (0.3, 0.4))
+
+        src2 = self._addFreq(src.copy(), (0.1, 0.4))
+        self._wrapFilter(src2, [(0.8, 0.8), (0.1, 0.2)], (0.8, 1.0), (0.3, 0.4))
+
 
 if __name__ == '__main__':
     ut.main()
