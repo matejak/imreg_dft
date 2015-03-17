53d95470e4d1432ae8b0c091f750290605c80e79
Matěj Týč <matej.tyc@gmail.com>
2015-03-17 18:40:01 +0100

    Fixed a tiling bug, made setup.py cwd-independent

--- src/imreg_dft/__init__.py
+++ src/imreg_dft/__init__.py
@@ -1,4 +1,12 @@
-from imreg_dft.imreg import *
+# -*- coding: utf-8 -*-
+
+# We may import this during setup invocation
+# because of the version we have to query
+# However, i.e. numpy may not be installed at setup install time.
+try:
+    from imreg_dft.imreg import *
+except ImportError as exc:
+    print("Unable to import the main package: %s" % exc.message)
 
 
 __version__ = "1.0.4-pre"
