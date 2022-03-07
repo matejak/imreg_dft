6b091a9817a6aa54a0fe6c9da11f15ef5220c756
EveCharbie <eve.charbonneau.1@umontreal.ca>
2022-03-07 17:13:59 -0500

    Fixed examples

--- resources/code/translation.py
+++ resources/code/translation.py
@@ -1,25 +1,26 @@
-import os
 
-import scipy as sp
-import scipy.misc
+import cv2
+import os, sys
+sys.path.append(os.path.join(os.path.dirname(__file__), "../../src/imreg_dft"))
+from imreg import (translation, similarity, transform_img, transform_img_dict, imshow)
 
-import imreg_dft as ird
-
-
-basedir = os.path.join('..', 'examples')
 # the TEMPLATE
-im0 = sp.misc.imread(os.path.join(basedir, "sample1.png"), True)
+im0_color = cv2.imread("../examples/sample1.png")
+im0 = cv2.cvtColor(im0_color, cv2.COLOR_RGB2GRAY)
+
 # the image to be transformed
-im1 = sp.misc.imread(os.path.join(basedir, "sample2.png"), True)
-result = ird.translation(im0, im1)
+im1_color = cv2.imread("../examples/sample2.png")
+im1 = cv2.cvtColor(im1_color, cv2.COLOR_RGB2GRAY)
+
+result = translation(im0, im1)
 tvec = result["tvec"].round(4)
 # the Transformed IMaGe.
-timg = ird.transform_img(im1, tvec=tvec)
+timg = transform_img(im1, tvec=tvec)
 
 # Maybe we don't want to show plots all the time
 if os.environ.get("IMSHOW", "yes") == "yes":
     import matplotlib.pyplot as plt
-    ird.imshow(im0, im1, timg)
+    imshow(im0, im1, timg)
     plt.show()
 
 print("Translation is {}, success rate {:.4g}"
